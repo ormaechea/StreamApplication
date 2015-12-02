843e0c65754b5cf7ef584ec8f2a3c5436ebffa46
ormaechea <castilloandres17@gmail.com>
2015-12-02 20:20:11 +0100

    Worked out a solution for streamer profile

--- app/controllers/streamers_controller.rb
+++ app/controllers/streamers_controller.rb
@@ -1,17 +1,11 @@
 class StreamersController < ApplicationController
   def index
-   @streamer_api_profile = WatchPeopleCodeApi::Stream.new.streamer_profile(params[:username])
-   @user = Streamer.new(username: @streamer_api_profile['data']['name'])
+   #This solution works but the API does not have a profile for live streams that are the most common.
+   #@streamer_api_profile = WatchPeopleCodeApi::Stream.new.streamer_profile(params[:username])
    # @user = Streamer.new(username: @streamer_api_profile["data"]["name"], twitch: @streamer_api_profile["data"]["twitch"], youtube: @streamer_api_profile["data"]["youtube"])
+   #So this is a workaroung
+   @user = Streamer.new(username: params[:username], twitch: params[:site], youtube: params[:id])
   end
-
-
-  def show
-    puts "----------------------------------------"
-    puts params
-    puts "----------------------------------------"
-  end
-
 
   # def new
   # end
