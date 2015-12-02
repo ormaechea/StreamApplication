1137c1f668a7fbbe9e8f4d1b99bc4aa1461461f4
ormaechea <castilloandres17@gmail.com>
2015-12-02 19:30:30 +0100

    Add Streamer profile controller logic and API call

--- app/controllers/streamers_controller.rb
+++ app/controllers/streamers_controller.rb
@@ -1,22 +1,24 @@
 class StreamersController < ApplicationController
   def index
+   @streamer_api_profile = WatchPeopleCodeApi::Stream.new.streamer_profile(params[:username])
+   @streamer_profile = Streamer.new(username: @streamer_api_profile["data"]["name"], twitch: @streamer_api_profile["data"]["twitch"], youtube: streamer_api_profile["data"]["youtube"])
   end
 
-  def show
-  end
+  # def show
+  # end
 
-  def new
-  end
+  # def new
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def create
-  end
+  # def create
+  # end
 
-  def update
-  end
+  # def update
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
 end
