3bb0563f67b021bb9029ab49d697634a71e92df2
ormaechea <castilloandres17@gmail.com>
2015-12-02 23:44:52 +0100

    Cleaning up the Application

--- app/controllers/streamers_controller.rb
+++ app/controllers/streamers_controller.rb
@@ -1,9 +1,5 @@
 class StreamersController < ApplicationController
   def index
-   #This solution works but the API does not have a profile for live streams that are the most common.
-   #@streamer_api_profile = WatchPeopleCodeApi::Stream.new.streamer_profile(params[:username])
-   # @user = Streamer.new(username: @streamer_api_profile["data"]["name"], twitch: @streamer_api_profile["data"]["twitch"], youtube: @streamer_api_profile["data"]["youtube"])
-   #So this is a workaround
    @user = Streamer.new(username: params[:username], twitch: params[:site], youtube: params[:id])
    #Live Streams ORM
     @live_streams = []
@@ -24,18 +20,5 @@ class StreamersController < ApplicationController
     end
   end
 
-  # def new
-  # end
 
-  # def edit
-  # end
-
-  # def create
-  # end
-
-  # def update
-  # end
-
-  # def destroy
-  # end
 end
--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -19,22 +19,4 @@ class StreamsController < ApplicationController
 
     end
 
-
-  # def show
-  # end
-
-  # def new
-  # end
-
-  # def edit
-  # end
-
-  # def create
-  # end
-
-  # def update
-  # end
-
-  # def destroy
-  # end
 end
